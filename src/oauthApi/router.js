d2918845e369ee3b19e630cc75d168f173d96291
ay <shelmafona@gmail.com>
2023-12-25 14:20:42 +0200

    routes pre configured

--- src/oauthApi/router.js
+++ src/oauthApi/router.js
@@ -4,12 +4,19 @@ async function oauthRouter(fastify, options) {
 	fastify.decorateRequest('user', '');
 	fastify.addHook('preHandler', authPreHandler);
 
+	// request new token pair
 	fastify.post('/device', async (request, reply) => {
-		return { oauth: 'GET DEVICE' };
+		return { oauth: 'POST REQUEST DEVICE TOKEN' };
 	});
 
+	// retrieve/refresh tokens pair
+	fastify.post('/token', async (request, reply) => {
+		return { oauth: 'POST RETRIEVE TOKENS' };
+	});
+
+	// get cached tokens
 	fastify.get('/token', async (request, reply) => {
-		return { oauth: 'GET TOKEN' };
+		return { oauth: 'GET DEVICE' };
 	});
 }
 
