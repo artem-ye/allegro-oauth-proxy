df9db5ab22652323c655efb1d584884b6283365a
ay <shelmafona@gmail.com>
2023-12-26 11:01:28 +0200

    rename oauthProxy

---
+++ src/oauthProxy/controller.js
@@ -0,0 +1,11 @@
+async function deviceController(user, params) {}
+
+async function tokensController(user, params) {}
+
+async function getTokensController(user, params) {}
+
+module.exports = {
+	deviceController,
+	tokensController,
+	getTokensController,
+};
