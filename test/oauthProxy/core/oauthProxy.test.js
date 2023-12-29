4f518454d81f7f920bdee3bce0faaf7a68c4ad65
ay <shelmafona@gmail.com>
2023-12-29 20:19:13 +0200

    db tests added

---
+++ test/oauthProxy/core/oauthProxy.test.js
@@ -0,0 +1,33 @@
+const OauthProxy = require('../../../src/oauthProxy/core/oauthProxy');
+const { db, close, models } = require('../../../src/oauthProxy/db/db');
+
+const testUserData = { client_id: 'test_user', client_secret: 'test_pass' };
+
+beforeAll(async () => {
+	await db();
+	await models.User.deleteOne(testUserData);
+});
+
+afterAll(async () => {
+	await models.User.deleteOne(testUserData);
+	await close();
+});
+
+describe('OauthProxy.js', () => {
+	it('createUser', async () => {
+		await OauthProxy.createUser(testUserData);
+		const res = await models.User.findOne(testUserData);
+		expect(res).not.toBeFalsy();
+	});
+
+	it('authorizedUser', async () => {
+		const res = await OauthProxy.authorizedUser({
+			client_id: '',
+			client_secret: '',
+		});
+		expect(res).toEqual(false);
+
+		const res2 = await OauthProxy.authorizedUser(testUserData);
+		expect(res2).toEqual(true);
+	});
+});
---
+++ test/oauthProxy/db/db.tokens.test.js
@@ -0,0 +1,54 @@
+const { db, close, models } = require('../../../src/oauthProxy/db/db');
+const { User: UserModel, Token: TokenModel } = models;
+
+beforeAll(async () => await db());
+afterAll(async () => await close());
+
+describe('db user model | saveTokens/getTokens', () => {
+	const testUser = {
+		client_id: 'foo',
+		client_secret: 'bar',
+	};
+
+	beforeAll(async () => await models.User.create(testUser));
+	afterAll(async () => {
+		await models.User.deleteOne(testUser);
+		await models.Token.deleteMany({ token: 'token' });
+	});
+
+	test('saveToken works', async () => {
+		const usr = models.User(testUser);
+		await usr.saveTokens({
+			token: 'token',
+			refresh_token: 'refresh_token',
+		});
+
+		// Can override without error
+		const res = await usr
+			.saveTokens({
+				token: 'token',
+				refresh_token: 'refresh_token',
+			})
+			.catch((err) => expect(err).toBe('error'));
+
+		const { user_id } = await models.User.find(testUser);
+		const updatedToken = await models.Token.find({ user_id });
+		expect(updatedToken).not.toBeFalsy();
+		expect(updatedToken.user_id).toEqual(user_id);
+
+		// Get tokens works
+		const userToken = await models.User({ _id: usr._id }).getTokens();
+		expect(userToken).not.toBeNull();
+		expect(userToken.user_id).toEqual(usr._id);
+	});
+
+	test('saveToken save unknown user token throws', async () => {
+		const usr = models.User({ ...testUser, client_id: 'fuck' });
+		const res = await usr
+			.saveTokens({
+				token: 'token',
+				refresh_token: 'refresh_token',
+			})
+			.catch((err) => expect(err).not.toBeNull());
+	});
+});
