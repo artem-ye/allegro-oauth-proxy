9c5edcff4846a2fae5a715c9bac3792c0efba567
ay <shelmafona@gmail.com>
2023-12-29 20:19:56 +0200

    config add test env

--- config/index.js
+++ config/index.js
@@ -1,9 +1,15 @@
+const mongo_url =
+	process.env.mongo_url ||
+	`mongodb://localhost:27017/Tokens` + process.env.NODE_ENV !== 'production'
+		? 'Test'
+		: '';
+
 const config = {
 	http: {
 		port: process.env.http_port || 3000,
 	},
 	mongo: {
-		url: process.env.mongo_url || `mongodb://localhost:27017/Tokens`,
+		url: process.env.MONGO_URL || `mongodb://localhost:27017/Tokens`,
 	},
 };
 
