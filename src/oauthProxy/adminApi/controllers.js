bab6c86e9516125a9504445d967d207b4e6456cc
ay <shelmafona@gmail.com>
2024-01-05 21:20:08 +0200

    finish user admin api

--- src/oauthProxy/adminApi/controllers.js
+++ src/oauthProxy/adminApi/controllers.js
@@ -1,6 +1,20 @@
+const UserModel = require('../db/model/UserModel');
+
 const adminApiHandlers = {
-	getUser: async function (req, res) {},
-	createUser: async function (req, res) {},
+	getUser: async function (req, res) {
+		const params = req.query;
+		const paramsPersist = !!Object.keys(params).length;
+
+		if (paramsPersist) {
+			return await UserModel.findOne(params);
+		} else {
+			return await UserModel.find({});
+		}
+	},
+	createUser: async function (req, res) {
+		const params = req.body;
+		return await UserModel.create(params);
+	},
 };
 
 module.exports = adminApiHandlers;
--- src/oauthProxy/adminApi/router.js
+++ src/oauthProxy/adminApi/router.js
@@ -1,3 +1,6 @@
+const handlers = require('./controllers');
+const config = require('../../../config');
+
 const routes = [
 	{
 		method: 'GET',
@@ -7,36 +10,40 @@ const routes = [
 				type: 'object',
 			},
 		},
-		handler: function (request, reply) {
-			reply.send({
-				admin: 'route',
-				method: request.method,
-				params: request.query,
-			});
-		},
+		handler: handlers.getUser,
 	},
 	{
 		method: 'POST',
 		url: '/',
 		schema: {
-			querystring: {
+			body: {
 				type: 'object',
-				// properties: {
-				// 	client_id: { type: 'string' },
-				// },
-				// required: ['client_id'],
+				properties: {
+					client_id: { type: 'string' },
+					client_secret: { type: 'string' },
+				},
+				required: ['client_id', 'client_secret'],
 			},
 		},
-		handler: function (request, reply) {
-			reply.send({ admin: 'route', method: request.method });
-		},
+		handler: handlers.createUser,
 	},
 ];
 
 async function oauthAdminRouter(fastify, options) {
-	// fastify.decorateRequest('client_id', '');
-	// fastify.decorateRequest('client_secret', '');
-	// fastify.addHook('preHandler', authPreHandler);
+	fastify.addHook('preHandler', async (req, res) => {
+		const authToken = req.headers.authorization;
+		const rootToken = config.api.root_token;
+
+		if (!rootToken) {
+			return res
+				.code(403)
+				.send({ error: 'Forbidden. Api auth disabled' });
+		}
+
+		if (authToken !== rootToken) {
+			return res.code(403).send({ error: 'Forbidden' });
+		}
+	});
 
 	for (const route of routes) {
 		fastify.route(route);
